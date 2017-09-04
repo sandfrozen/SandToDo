a79569c28c2a142cead4b8d6de3f906daa98f43f
Tomek Buslowski <tbuslowski@gmail.com>
2017-09-04 15:02:19 +0200

    lists view, todolist view and controllers to both

---
+++ TodoListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TodoListViewController.swift
+//  SandToDo
+//
+//  Created by Tomek Buslowski on 04.09.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+    
+    var todos = [Todo]()
+
+    @IBOutlet weak var addNewTextFiled: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+    }
+}
